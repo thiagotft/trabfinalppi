f3bc88f6aa2c70bf3458612db4dd1caae31e7819
Cairo Cruz <cruzcairo@gmail.com>
2024-03-05 12:39:50 -0300

    Implement employee and patient registration and listing functionality

---
+++ TrabFinalPPI/styleGlobal/footerAndHeaderPrivate.js
@@ -0,0 +1,7 @@
+fetch('/TrabFinalPPI/styleGlobal/headerPrivate.html')
+.then(response => response.text())
+.then(data => document.getElementById('header').innerHTML = data);
+
+fetch('/TrabFinalPPI/styleGlobal/footer.html')
+.then(response => response.text())
+.then(data => document.getElementById('footer').innerHTML = data);
