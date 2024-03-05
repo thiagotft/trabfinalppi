c37efe3b9a0cf93954f7acc203cf647395ecf798
Cairo Cruz <cruzcairo@gmail.com>
2024-03-05 11:52:42 -0300

    Project folder reorganization

--- TrabFinalPPI/styleGlobal/footerAndHeader.js
+++ TrabFinalPPI/styleGlobal/footerAndHeader.js
@@ -1,7 +1,7 @@
-fetch('/TrabFinalPPI/Parte1/styleGlobal/header.html')
+fetch('/TrabFinalPPI/styleGlobal/header.html')
 .then(response => response.text())
 .then(data => document.getElementById('header').innerHTML = data);
 
-fetch('/TrabFinalPPI/Parte1/styleGlobal/footer.html')
+fetch('/TrabFinalPPI/styleGlobal/footer.html')
 .then(response => response.text())
 .then(data => document.getElementById('footer').innerHTML = data);
