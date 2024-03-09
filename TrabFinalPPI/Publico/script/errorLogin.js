f2487c82bbb9bae821b0d35ee05d35e3213292ea
rafaeldutrabasso <rafael.dutra@ufu.br>
2024-03-08 23:20:41 -0300

    asdaa

--- TrabFinalPPI/Publico/script/errorLogin.js
+++ TrabFinalPPI/Publico/script/errorLogin.js
@@ -17,6 +17,13 @@ document.addEventListener("DOMContentLoaded", function () {
             let html1 = "Insira a senha";
             span2.innerHTML = html1;
         }
-       
+        if (email.value !== "" && senha.value !== "") {
+           
+            if (email.value === "exemplo@email.com" && senha.value === "senha123") {
+                alert("Login bem-sucedido!"); 
+            } else {
+                alert("Login e/ou senha incorretos!"); 
+            }
+        }
     });
 });
