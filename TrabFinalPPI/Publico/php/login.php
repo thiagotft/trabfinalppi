790672d1cd5183ba9d4b3b0e6507be689467ab27
Thiago Fernandes <tfpavani@gmail.com>
2024-04-16 16:19:39 -0300

    Create login.php

---
+++ TrabFinalPPI/Publico/php/login.php
@@ -0,0 +1,36 @@
+<?php
+require "TrabFinalPPI/connection.php";
+
+$acao = $_GET['acao'];
+
+$pdo = getConnection();
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT f.SenhaHash, p.Email
+            FROM Funcionário f
+            JOIN Pessoa p ON f.PessoaId = p.Id
+            WHERE p.Email = ?";
+    
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$email]);
+
+    if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $storedHash = $row['SenhaHash'];
+        
+        if (password_verify($password, $storedHash)) {
+  
+            echo json_encode(['status' => 'success', 'message' => 'Login realizado com sucesso.']);
+        } else {
+
+            echo json_encode(['status' => 'error', 'message' => 'Senha incorreta.']);
+        }
+    } else {
+
+        echo json_encode(['status' => 'error', 'message' => 'Usuário não encontrado.']);
+    }
+}
+?>
