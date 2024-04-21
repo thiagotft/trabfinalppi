fbd3b91eefd12f2bb051520308299e0349940d0b
Thiago Fernandes <tfpavani@gmail.com>
2024-04-21 15:37:11 -0300

    Update login

--- TrabFinalPPI/Publico/php/login.php
+++ TrabFinalPPI/Publico/php/login.php
@@ -1,36 +1,29 @@
 <?php
-require "TrabFinalPPI/connection.php";
-require "../../connection.php";
+require '../connection.php';
 $pdo = getConnection();
 
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+$sql = <<<SQL
+    SELECT f.senhahash
+    FROM funcionario f
+    JOIN pessoa p ON f.codigo = p.codigo
+    WHERE p.email = :email
+SQL;
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute([':email' => $email]);
+
+if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $storedHash = $row['senhahash'];
     
-    $sql = <<<SQL
-            SELECT f.senhahash
-            FROM funcionario f
-            JOIN pessoa p ON f.codigo = p.codigo
-            WHERE p.email = :email
-            SQL;
-    
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([':email' => $email]);
-    if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $storedHash = $row['senhahash'];
-        
-        if (password_verify($password, $storedHash)) {
-            echo json_encode(['status' => 'success', 'message' => 'Login realizado com sucesso.']);
-            header("Location:listaAgend.html");
-    exit();
-        } else {
-            echo json_encode(['status' => 'error', 'message' => 'Senha incorreta.']);
-        }
+    if (password_verify($password, $storedHash)) {
+        echo "Login realizado com sucesso.";
     } else {
-        echo json_encode(['status' => 'error', 'message' => 'Usuário não encontrado.']);
-    }
-header("Location:listaAgend.html");
-    exit();
+        echo "Senha incorreta.";
+    }
+} else {
+    echo "Usuário não encontrado.";
 }
 ?>
