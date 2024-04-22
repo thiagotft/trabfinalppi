f04dd3495329cbc7fd4f8a3388f29da7e420ad93
rafaeldutrabasso <rafael.dutra@ufu.br>
2024-04-21 22:06:23 -0300

    listagem de enderecos OK

--- TrabFinalPPI/Restrito/mostrar-endereco.php
+++ TrabFinalPPI/Restrito/mostrar-endereco.php
@@ -1,73 +1,75 @@
 <?php
 require '../connection.php';
 $pdo = getConnection();
+
 try {
-  $sql = <<<SQL
-  SELECT cep, logradouro, cidade, estado
-  FROM endereco
-  LIMIT 30
-  SQL;
+    $sql = <<<SQL
+    SELECT * FROM endereco
+    SQL;
+
     $stmt = $pdo->query($sql);
 } catch (Exception $e) {
-    exit('Ocorreu uma falha ao listar os endereços: ' . $e->getMessage());
+    exit('Ocorreu uma falha ao listar os agendamentos: ' . $e->getMessage());
 }
 ?>
+
 <!DOCTYPE html>
-<html lang="pt-BR">
-  <head>
-    <meta charset="UTF-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
+<html lang="en">
 
-    <title>Listagem de Endereços Cadastrados</title>
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-      crossorigin="anonymous"
-    />
-    <link rel="stylesheet" href="/TrabFinalPPI/Publico/css/styleEnd.css" type="text/css" media="screen">
-  <title>Lista de Endereços</title>
-
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="/TrabFinalPPI/Restrito/css/styleList.css" type="text/css" media="screen">
+    <title>Lista de Paciente</title>
+    
 </head>
 
 <body>
     <div id="header"></div>
     <div id="conteudo">
-        <h2>Endereços Cadastrados</h2>
+        <h2>
+            Lista de pacientes
+        </h2>
         <div class="table-responsive">
         <table class="table table-striped table-info table-hover">
-          <tr>
-            <th></th>
-            <th>CEP</th>
-            <th>Logradouro</th>
-            <th>Cidade</th>
-            <th>Estado</th>
-          </tr>
-          <?php
-          while($row = $stmt->fetch()){
-            $cep = htmlspecialchars($row['cep']);
-            $logradouro = htmlspecialchars($row['logradouro']);
-            $cidade = htmlspecialchars($row['cidade']);
-            $estado = htmlspecialchars($row['estado']);
 
-            echo <<<HTML
-              <tr>
-              <th scope="row"></th>
-              <td>$cep</td>
-              <td>$logradouro</td>
-              <td>$cidade</td>
-              <td>$estado</td>
-            </tr>
-            HTML;
-          }
-          ?>
+            <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">CEP</th>
+                  <th scope="col">Logradouro</th>
+                  <th scope="col">Cidade</th>
+                  <th scope="col">Estado</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                while($row = $stmt->fetch()){
+                $rowNumber = htmlspecialchars($row['id']);
+                $cep = htmlspecialchars($row['cep']);
+                $log = htmlspecialchars($row['logradouro']);
+                $cidade = htmlspecialchars($row['cidade']);
+                $estado = htmlspecialchars($row['estado']);
+                
+                echo <<<HTML
+                     <tr>
+                     <th scope="row">$rowNumber</th>
+                     <td>$cep</td>
+                     <td>$log</td>
+                     <td>$cidade</td>
+                     <td>$estado</td>
+                     </tr>
+                     HTML;
+                }
+                ?>
+            </tbody>
         </table>
         </div>
     </div>
     <div id="footer"></div>
     <script src="/TrabFinalPPI/styleGlobal/footerAndHeaderPrivate.js"></script>
-  </body>
+</body>
+
 </html>
